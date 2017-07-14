30d69f6746bb89e1d13095f4285db78db127281f
jonsequitur <jonsequeira@gmail.com>
2017-07-14 11:54:12 -0700

    add support for discovery of Pocket.Logger in other assemblies

--- Pocket.Logger/Core/Log.Subscribe.cs
+++ Pocket.Logger/Core/Log.Subscribe.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
 
 namespace Pocket
 {
@@ -27,6 +30,33 @@ namespace Pocket
             Logger.EntryPosted += LoggerOnEntryPosted;
 
             return Disposable.Create(() => { Logger.EntryPosted -= LoggerOnEntryPosted; });
+        }
+
+        public static IDisposable DiscoverAndSubscribe(
+            Action<IReadOnlyCollection<KeyValuePair<string, object>>> onEntryPosted)
+        {
+            var thisAssembly = typeof(Log).GetTypeInfo().Assembly;
+
+            var logTypeName = typeof(Logger).FullName;
+
+            var loggerTypes = Discover.ConcreteTypes()
+                                      .Where(t => !t.GetTypeInfo()
+                                                    .Assembly
+                                                    .Equals(thisAssembly))
+                                      .Where(t => t.FullName == logTypeName);
+
+            var disposables = new CompositeDisposable();
+
+            foreach (var loggerType in loggerTypes)
+            {
+                var entryPosted = (EventInfo) loggerType.GetMember(nameof(Logger.EntryPosted)).Single();
+
+                entryPosted.AddEventHandler(null, onEntryPosted);
+
+                disposables.Add(Disposable.Create(() => { entryPosted.RemoveEventHandler(null, onEntryPosted); }));
+            }
+
+            return disposables;
         }
     }
 }
