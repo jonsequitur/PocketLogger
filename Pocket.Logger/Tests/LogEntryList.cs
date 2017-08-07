2b2f846d7d0fa37c04ff7d28db798ef07ae3b720
jonsequitur <jonsequeira@gmail.com>
2017-08-07 14:27:57 -0700

    make LogEntryList threadsafe

--- Pocket.Logger/Tests/LogEntryList.cs
+++ Pocket.Logger/Tests/LogEntryList.cs
@@ -1,9 +1,10 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
 
 namespace Pocket.Tests
 {
-    public class LogEntryList : List<(
+    public class LogEntryList : ConcurrentQueue<(
         byte LogLevel,
         DateTime TimestampUtc,
         Func<(string Message, (string Name, object Value)[] Properties)> Evaluate,
@@ -16,5 +17,33 @@ namespace Pocket.Tests
         bool? IsSuccessful,
         TimeSpan? Duration) Operation)>
     {
+        public void Add(
+            (
+                byte LogLevel,
+                DateTime TimestampUtc,
+                Func<(string Message, (string Name, object Value)[] Properties)> Evaluate,
+                Exception Exception,
+                string OperationName,
+                string Category,
+                (string Id,
+                bool IsStart,
+                bool IsEnd,
+                bool? IsSuccessful,
+                TimeSpan? Duration) Operation) e) =>
+            Enqueue(e);
+
+        public (
+            byte LogLevel,
+            DateTime TimestampUtc,
+            Func<(string Message, (string Name, object Value)[] Properties)> Evaluate,
+            Exception Exception,
+            string OperationName,
+            string Category,
+            (string Id,
+            bool IsStart,
+            bool IsEnd,
+            bool? IsSuccessful,
+            TimeSpan? Duration) Operation) this[int index] =>
+            this.ElementAt(index);
     }
 }
