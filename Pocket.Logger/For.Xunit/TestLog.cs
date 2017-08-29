a6c6d3b9bde62e75205f91aee8224690ad3b38a5
jonsequitur <jonsequeira@gmail.com>
2017-08-29 12:59:21 -0700

    attribute-based xunit test support

---
+++ Pocket.Logger/For.Xunit/TestLog.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace Pocket.For.Xunit
+{
+    internal class TestLog : IDisposable
+    {
+        private readonly CompositeDisposable disposables = new CompositeDisposable();
+
+        private readonly ConcurrentQueue<string> text = new ConcurrentQueue<string>();
+
+        public TestLog(
+            MethodInfo testMethod,
+            bool writeToFile = false,
+            string filename = null)
+        {
+            if (testMethod == null)
+            {
+                throw new ArgumentNullException(nameof(testMethod));
+            }
+
+            var testName = $"{testMethod.DeclaringType.Name}.{testMethod.Name}";
+
+            disposables.Add(Disposable.Create(() =>
+            {
+                Log.Dispose();
+            }));
+
+            if (writeToFile)
+            {
+                filename = filename ??
+                           $"{testName}-{DateTime.Now:yyyy-MM-dd-hh-mm-ss}.log";
+                LogFile = new FileInfo(filename);
+
+                LogToFile();
+            }
+
+            disposables.Add(
+                LogEvents.Subscribe(e => Write(e.ToLogString())));
+
+            TestName = testName;
+
+            Log = new OperationLogger(TestName, logOnStart: true);
+        }
+
+        private void LogToFile()
+        {
+            var stream = LogFile.AppendText();
+
+            disposables.Add(
+               LogEvents.Subscribe(e =>
+                    stream.WriteLine(e.ToLogString())));
+
+            disposables.Add(Disposable.Create(() => stream.Dispose()));
+        }
+
+        public OperationLogger Log { get; }
+
+        public FileInfo LogFile { get; }
+
+        public string TestName { get; }
+
+        public IEnumerable<string> Text => text;
+
+        public void Write(string text) => this.text.Enqueue(text);
+
+        private static readonly AsyncLocal<TestLog> current = new AsyncLocal<TestLog>();
+
+        public static TestLog Current
+        {
+            get => current.Value;
+            set => current.Value = value;
+        }
+
+        public void Dispose()
+        {
+            Current = null;
+            disposables.Dispose();
+        }
+    }
+}
---
+++ Pocket.Logger/For.Xunit/Tests/HowAreLogsAccessed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Pocket.For.Xunit.Tests
+{
+    [LogToPocketLogger]
+    public class HowAreLogsAccessed : IDisposable
+    {
+        private readonly CompositeDisposable disposables = new CompositeDisposable();
+
+        public HowAreLogsAccessed(ITestOutputHelper output)
+        {
+            LogEvents.Subscribe(e => output.WriteLine(e.ToLogString()));
+        }
+
+        public void Dispose() => disposables.Dispose();
+
+        [Fact]
+        public void Log_entries_written_to_PocketLogger_are_captured_by_the_test_log()
+        {
+            Logger.Log.Info("hi!");
+
+            TestLog.Current
+                   .Text
+                   .Should()
+                   .Contain(line => line.Contains("hi!"));
+        }
+
+        [Fact]
+        public void The_test_can_write_directly_to_the_test_log()
+        {
+            TestLog.Current.Log.Info("hello!");
+
+            TestLog.Current
+                   .Text
+                   .Should()
+                   .Contain(line => line.Contains("hello!"));
+        }
+    }
+}
