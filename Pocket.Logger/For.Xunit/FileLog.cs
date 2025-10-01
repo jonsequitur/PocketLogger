52da877a1256e9ee6c84220795e65bec41e3ba38
Jon Sequeira <jonsequeira@gmail.com>
2025-10-01 13:34:29 -0700

    fix xunit double write bug, support specifying assemblies in attribute

--- Pocket.Logger/For.Xunit/FileLog.cs
+++ Pocket.Logger/For.Xunit/FileLog.cs
@@ -3,14 +3,17 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Threading;
+using LogEvent = (string MessageTemplate, object[]? Args, System.Collections.Generic.List<(string Name, object? Value)> Properties, byte LogLevel, System.DateTime TimestampUtc, System.Exception? Exception, string? OperationName, string? Category, (string? Id, bool IsStart, bool IsEnd, bool? IsSuccessful, System.TimeSpan? Duration) Operation);
 
 namespace Pocket.For.Xunit;
 
-internal class FileLog
+internal class FileLog : IDisposable
 {
     private readonly CompositeDisposable _disposables = new();
 
-    private static readonly object _lockObj = new();
+    private static readonly Lock _lockObj = new();
 
     public FileLog(string filename)
     {
@@ -18,19 +21,9 @@ internal class FileLog
         SubscribeFileLog();
     }
 
-    private void SubscribeFileLog()
-    {
-        _disposables.Add(
-            LogEvents.Subscribe(e =>
-            {
-                var entry = e.ToLogString() + Environment.NewLine;
+    public void Dispose() => _disposables.Dispose();
 
-                lock (_lockObj)
-                {
-                    System.IO.File.AppendAllText(File.FullName, entry);
-                }
-            }));
-    }
+    private void SubscribeFileLog() => _disposables.Add(LogEvents.Subscribe(WriteLogEntry));
 
     public FileInfo File { get; }
 
@@ -43,6 +36,18 @@ internal class FileLog
                 using var streamReader = new StreamReader(File.OpenRead());
                 return streamReader.ReadToEnd().Trim().Split(['\n', '\r']);
             }
+        }
+    }
+
+    public void Subscribe(Assembly[] assemblies) => _disposables.Add(LogEvents.Subscribe(WriteLogEntry,assemblies));
+
+    private void WriteLogEntry(LogEvent e)
+    {
+        var entry = e.ToLogString() + Environment.NewLine;
+
+        lock (_lockObj)
+        {
+            System.IO.File.AppendAllText(File.FullName, entry);
         }
     }
 }
